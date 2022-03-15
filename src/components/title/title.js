63abe944fbd974e9b103ffd42aad35ee8edb84ff
Tyler Dansby <tdansby7@gmail.com>
2022-03-15 16:13:41 -0500

    no follow wiki link

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -35,7 +35,7 @@ function Title() {
           <Typography>
             Pomodoro (Italian for tomato) is a study technique that takes advantage of switching between work periods and break  periods to maximize focus and minimize distractions. The timer below allows you to customize the length of your work and rest intervals and set the total number of rounds. The typical breakdown is 4 rounds with 25 minute work periods followed by 5-10 minute break periods. 
           </Typography>
-          <Link href="https://en.wikipedia.org/wiki/Pomodoro_Technique" variant="h6" target="_blank" color="primary">Learn More about Pomodoro</Link>
+          <Link rel="nofollow" href="https://en.wikipedia.org/wiki/Pomodoro_Technique" variant="h6" target="_blank" color="primary">Learn More about Pomodoro</Link>
           <Typography>
             Pick a theme that fits your study mood, and you can also copy and paste a YouTube URL at the bottom of the page to embed whatever kind of videos help you study, (I’m a big fan of Lofi music streams personally). Happy Studying!
           </Typography>
